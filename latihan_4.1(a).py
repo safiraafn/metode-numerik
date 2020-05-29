b5155e1f879274696ec2b1890083d956c47a119c
Safira Fauzianingsih <61137134+safiraafn@users.noreply.github.com>
2020-05-29 11:33:42 +0800

    Latihan 4.1 bagian a Bab 4

---
+++ latihan_4.1(a).py
@@ -0,0 +1,10 @@
+import numpy as np 
+from numpy import array, zeros
+A = array([[0.,2.,0.,1], [2.,2.,3.,2], [4.,-3.,0.,1], [6.,1.,-6.,-5]]) 
+
+B = array([[0],[-2],[-7],[6]])
+
+print('A =')
+print(A)
+print('B=')
+print(B)
